74377316985c51ab7def5d047ade8c954b336241
Jeremie Pivot <jeremiepivot@yopmail.fr>
2017-05-10 16:29:19 +0200

    last update bis commit

--- filtre/celKelFilter.js
+++ filtre/celKelFilter.js
@@ -3,7 +3,6 @@ function celKelFilter(){
         if (cel==true){
         celcuis = temperature - 273,15;
         deg = celcuis.toFixed(2);
-        console.log(cel);
         return (deg +"°C");}
         else{
             return (temperature+ "°K");
